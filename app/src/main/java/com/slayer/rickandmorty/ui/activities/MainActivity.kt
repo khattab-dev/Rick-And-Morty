e9ab2fb02fe27baac1725aa6ec673a4f1e468ae3
Ahmed Khaled <akhaled.vi@gmail.com>
2024-03-30 03:03:32 +0200

    Added bottom nav bar

--- app/src/main/java/com/slayer/rickandmorty/ui/activities/MainActivity.kt
+++ app/src/main/java/com/slayer/rickandmorty/ui/activities/MainActivity.kt
@@ -5,19 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.slayer.rickandmorty.R
+import com.slayer.rickandmorty.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     private val TAG = MainActivity::class.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment = binding.fragmentContainerView.getFragment() as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
